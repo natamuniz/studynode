c40c98efc4a97a0e983e520de60b3f2b187077ca
Natã Muniz <natan.m25@gmail.com>
2023-01-17 07:26:19 -0300

    esqueci de salvar antes de commitar

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ const express = require('express');
 
 const server = express();
 
+const cursos = ['Nodejs', 'JavaScript', 'React Native'];
+
 //localhost:3000/curso
-server.get('/curso/:id', (req, res) => {
+server.get('/curso/: index', (req, res) => {
    
-    const id = req.params.id;
+    const {index} = req.params;
    
-    return res.json({curso: `Curso: ${id}`});
+    return res.json(cursos[index]);
 })
 
 server.listen(3000);
