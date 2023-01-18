457c39124c230cddeedd3043dd6f778a8b0e74dc
Natã Muniz <natan.m25@gmail.com>
2023-01-18 07:55:36 -0300

    Reinicializando

---
+++ modules/fs.js
@@ -0,0 +1,24 @@
+const fs = require("fs");
+const path = require("path");
+
+// Criar uma pasta
+fs.mkdir(path.join(__dirname, "/test"), (error) => {
+  if (error) {
+    return console.log("Erro: ", error);
+  }
+
+  console.log("Pasta criada com sucesso");
+});
+
+// Criar um arquivo
+fs.writeFile(
+  path.join(__dirname, "/test", "test.txt"),
+  "hello node!",
+  (error) => {
+    if (error) {
+      return console.log("Erro ", error);
+    }
+
+    console.log("Arquivo criado com sucesso");
+  }
+);
