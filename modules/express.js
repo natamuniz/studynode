2f4e89c432a739ec90449322def05939625c5d13
Natã Muniz <natan.m25@gmail.com>
2023-01-23 20:36:32 -0300

    update database

--- modules/express.js
+++ modules/express.js
@@ -1,7 +1,8 @@
 const express = require("express");
+const UserModel = require("../src/models/user.model");
 
 const app = express();
-
+app.use(express.json());
 app.get("/home", (req, res) => {
   res.contentType("application/html");
   res.status(200).send("<h1>Hello world</h1>");
@@ -19,6 +20,16 @@ app.get("/users", (req, res) => {
     },
   ];
   res.status(200).json(users);
+});
+
+app.post("/users", async (req, res) => {
+  try {
+    const user = await UserModel.create(req.body);
+
+    res.status(201).json(user);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
 });
 
 const port = 8080;
