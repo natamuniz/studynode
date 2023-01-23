c8ad29ecac3c50c87e483c58bd4f19fd68fabab6
Natã Muniz <natan.m25@gmail.com>
2023-01-23 07:57:30 -0300

    Connecting database

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const { Person } = require("./person");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 //require("./modules/path");
 //require("./modules/fs");
-require("./modules/http");
+//require("./modules/http");
+require("./modules/express");
 
 const person = new Person("Natã");
