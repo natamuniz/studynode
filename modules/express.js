b541cc5e77e24c49e6f39682563b06a5bae94f82
Natã Muniz <natan.m25@gmail.com>
2023-01-25 18:12:24 -0300

    Adding delete fuction

--- modules/express.js
+++ modules/express.js
@@ -18,7 +18,7 @@ app.get("/users", async (req, res) => {
   }
 });
 
-app.get("user/:id", async (req, res) => {
+app.get("/users/:id", async (req, res) => {
   try {
     const id = req.params.id;
 
@@ -35,6 +35,29 @@ app.post("/users", async (req, res) => {
     const user = await UserModel.create(req.body);
 
     res.status(201).json(user);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
+app.patch("/users/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+
+    const user = await UserModel.findByIdAndUpdate(id, req.body, { new: true });
+
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
+app.delete("/users/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const user = await UserModel.findByIdAndRemove(id);
+
+    res.status(200).json(user);
   } catch (error) {
     res.status(500).send(error.message);
   }
